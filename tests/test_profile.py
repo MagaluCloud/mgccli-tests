8729b6e9828d02cdca7477057b5b105a0d60f324
Luiz Cavalcanti <luiz.cavalcanti@luizalabs.com>
2025-06-03 11:46:18 -0300

    feat: add invalid ssh key name test

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -2,6 +2,8 @@ from utils import run_cli
 
 test_profile_context = {}
 
+PUBLIC_KEY = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK0wmN/Cr3JXqmLW7u+g9pTh+wyqDHpSQEIQczXkVx9q not_really@a.key"
+
 
 def test_profile_availability_zones_list():
     exit_code, _, _, jsonout = run_cli(["profile", "availability_zones", "list"])
@@ -11,13 +13,20 @@ def test_profile_availability_zones_list():
 
 
 def test_profile_ssh_keys_create():
-    key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK0wmN/Cr3JXqmLW7u+g9pTh+wyqDHpSQEIQczXkVx9q not_really@a.key"
     exit_code, _, _, jsonout = run_cli(
-        ["profile", "ssh-keys", "create", "--name=cli-test-key", f"--key={key}"]
+        ["profile", "ssh-keys", "create", "--name=cli-test-key", f"--key={PUBLIC_KEY}"]
     )
     assert exit_code == 0
     assert "id" in jsonout
     test_profile_context["key_id"] = jsonout["id"]
+
+
+def test_profile_ssh_keys_create_fails_with_invalid_chars():
+    exit_code, _, stderr, _ = run_cli(
+        ["profile", "ssh-keys", "create", "--name=key.with.dots", f"--key={PUBLIC_KEY}"]
+    )
+    assert exit_code != 0, stderr
+    assert "Status: 422 Unprocessable Entity" in stderr
 
 
 def test_profile_ssh_keys_list():
